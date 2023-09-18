572ffafe3e9a6e227c8a70c58abbac0b6ab97b47
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-09-18 20:36:06 +0000

    add Rot13 function

--- basic_format.c
+++ basic_format.c
@@ -47,3 +47,40 @@ int handle_basic_formats(char *buffer, const char specifier, va_list args)
 	/*current index of the buffer*/
 	return (index);
 }
+
+/**
+  * handle_custom_formats - this function should handle the rot13
+  * and reversed string
+  * @buffer: buffer to store the the value of the specifier
+  * @specifier: define the exact format (s,c,%) to be printed
+  * @args: list that hold the values
+  * Return: length of the stored charachters
+  */
+int handle_custom_formats(char *buffer, const char specifier, va_list args)
+{
+	int index = 0;
+	char *str;
+
+	switch (specifier)
+	{
+		case 'R':
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+				{
+					str = "(null)";
+				}
+				else
+				{
+					str = _rot13(str);
+				}
+				while (*str)
+				{
+					buffer[index++] = *str++;
+				}
+			}
+			break;
+	}
+	return (index);
+}
+
--- main.h
+++ main.h
@@ -12,6 +12,8 @@ int _strlen(char *str);
 char *_itoa(int value, char *str);
 char *_utoa(unsigned long value, char *str, int base);
 char *_toUpper(char *c);
+char *_rot13(char *c);
+
 int reverse_string_with_len(char *buffer, char *str);
 void reverse_string_without_len(char *start, char *end);
 int handle_basic_formats(char *buffer, const char specifier, va_list args);
@@ -19,6 +21,7 @@ int handle_integer_binary(char *buffer, const char specifier, va_list args);
 int handle_octal_hexa(char *buffer, const char specifier, va_list args);
 int handle_unsigned_pointer(char *buffer, const char specifier, va_list args);
 int process_format(const char *format, int *i, va_list args, char *buffer);
+int handle_custom_formats(char *buffer, const char specifier, va_list args);
 int _printf(const char *format, ...);
 
 #endif
