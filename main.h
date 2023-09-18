1f9d12367940d8ca62f830efeb6a1fbd9e8b1386
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-09-18 02:15:18 +0000

    add main.h file

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define BUFFER_SIZE 1024
+#include <stdarg.h>
+#include <stddef.h>
+#include <unistd.h>
+
+char *_strchr(const char *s, int c);
+int _strlen(char *str);
+char *_itoa(int value, char *str);
+char *_toUpper(char *c);
+int reverse_string_with_len(char *buffer, const char *str);
+void reverse_string_without(char *start, char *end);
+int handle_basic_formats(char *buffer, const char specifier, va_list args);
+int handle_numbers(char *buffer, const char specifier, va_list args);
+int _printf(const char *format, ...);
+
+#endif
