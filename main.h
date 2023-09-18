97361fa99f676135d049fb43ef5479dd897acc02
MemanAwad <meman1awad@gmail.com>
2023-09-18 16:55:42 +0200

    add new function to main.h

--- main.h
+++ main.h
@@ -13,8 +13,9 @@ char *_toUpper(char *c);
 int reverse_string_with_len(char *buffer, const char *str);
 void reverse_string_without_len(char *start, char *end);
 int handle_basic_formats(char *buffer, const char specifier, va_list args);
-int handle_integer_and_binary(char *buffer, const char specifier, va_list args);
-int handle_octal_and_hexa(char *buffer, const char specifier, va_list args);
+int handle_integer_binary(char *buffer, const char specifier, va_list args);
+int handle_octal_hexa(char *buffer, const char specifier, va_list args);
+int handle_unsigned_pointer(char *buffer, const char specifier, va_list args);
 int _printf(const char *format, ...);
 
 #endif
