c6bb2fb8d71cede7d90260c7cced9896247d181e
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-09-18 14:02:54 +0000

    update revereve without len name in header

--- main.h
+++ main.h
@@ -11,7 +11,7 @@ int _strlen(char *str);
 char *_itoa(int value, char *str);
 char *_toUpper(char *c);
 int reverse_string_with_len(char *buffer, const char *str);
-void reverse_string_without(char *start, char *end);
+void reverse_string_without_len(char *start, char *end);
 int handle_basic_formats(char *buffer, const char specifier, va_list args);
 int handle_integer_and_binary(char *buffer, const char specifier, va_list args);
 int handle_octal_and_hexa(char *buffer, const char specifier, va_list args);
