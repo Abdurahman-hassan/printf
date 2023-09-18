c25d9f1655f0f5809255dd2dbe0138812c607766
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-09-18 15:34:14 +0000

    seperate the funtion into two functions to make it readable

--- _printf.c
+++ _printf.c
@@ -1,20 +1,61 @@
 #include "main.h"
 
 /**
-  * _printf - This function is simlute the original printf
-  * This function check after % and call the sutable function
-  * these functions need a buffer to store the chars into it
-  * and the format cs%uxXou ... etc and the args that take
-  * after convinsion from function throght va_arg
-  * @format: the string include csuxXo%
-  * @...: this is allow to us to add multible convinsion
-  * Return: length
-  */
+ * process_format - this function reposable for handling
+ * the format in order to make the code readable and clean
+ * @format: this is string that we will use
+ * @i: integer number to use in loop
+ * @args: this is the input that takes after convinsion
+ * @buffer: the array that we will use to store the strings into it
+ * Return: the index
+ */
+int process_format(const char *format, int *i, va_list args, char *buffer)
+{
+	int buffer_index = 0;
+
+	if (_strchr("cs%", format[i + 1]))
+	{
+		buffer_index +=
+			handle_basic_formats(buffer + buffer_index, format[i + 1], args);
+		*(i)++;
+	}
+	else if (_strchr("dib", format[i + 1]))
+	{
+		buffer_index +=
+			handle_integer_binary(buffer + buffer_index, format[i + 1], args);
+		*(i)++;
+	}
+	else if (_strchr("xoX", format[i + 1]))
+	{
+		buffer_index +=
+			handle_octal_hexa(buffer + buffer_index, format[i + 1], args);
+		*(i)++;
+	}
+	else if (_strchr("up", format[i + 1]))
+	{
+		buffer_index +=
+			handle_unsigned_pointer(buffer + buffer_index, format[i + 1], args);
+		*(i)++;
+	}
+	return (buffer_index);
+
+}
+
+
+/**
+ * _printf - This function is simlute the original printf
+ * This function check after % and call the sutable function
+ * these functions need a buffer to store the chars into it
+ * and the format cs%uxXou ... etc and the args that take
+ * after convinsion from function throght va_arg
+ * @format: the string include csuxXo%
+ * @...: this is allow to us to add multible convinsion
+ * Return: length
+ */
 int _printf(const char *format, ...)
 {
 	int i;
 	char buffer[BUFFER_SIZE];
-	int buffer_index = 0;
 	va_list args;
 
 	va_start(args, format);
@@ -24,30 +65,7 @@ int _printf(const char *format, ...)
 		/* check after % if thers csdiouxX and not empty */
 		if (format[i] == '%' && format[i + 1] != '\0')
 		{
-			if (_strchr("cs%", format[i + 1]))
-			{
-				buffer_index +=
-					handle_basic_formats(buffer + buffer_index, format[i + 1], args);
-				i++;
-			}
-			else if (_strchr("dib", format[i + 1]))
-			{
-				buffer_index +=
-					handle_integer_binary(buffer + buffer_index, format[i + 1], args);
-				i++;
-			}
-			else if (_strchr("xoX", format[i + 1]))
-			{
-				buffer_index +=
-					handle_octal_hexa(buffer + buffer_index, format[i + 1], args);
-				i++;
-			}
-			else if (_strchr("up", format[i + 1]))
-			{
-				buffer_index +=
-					handle_unsigned_pointer(buffer + buffer_index, format[i + 1], args);
-				i++;
-			}
+			buffer_index += process_format(format, &i, arg, buffer + buffer_index);
 		}
 		else
 		{
