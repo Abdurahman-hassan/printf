39ae57b46b333fecb630bbb7e9ba1b2c41c2e0a9
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-09-19 19:03:55 +0000

    add flag to functions

--- main.h
+++ main.h
@@ -11,20 +11,20 @@
 
 char *_strchr(const char *s, int c);
 int _strlen(char *str);
-char *_itoa(int value, char *str);
-char *_utoa(unsigned long value, char *str, int base);
+char *_itoa(int value, char *str, char flag);
+char *_utoa(unsigned long value, char *str, int base, char flag);
 char *_toUpper(char *c);
 char *_rot13(char *c);
 
 int reverse_string_with_len(char *buffer, char *str);
 void reverse_string_without_len(char *start, char *end);
 int handle_basic_formats(char *buffer, const char specifier, va_list args);
-int handle_integer_binary(char *buffer, const char specifier, va_list args);
-int handle_octal_hexa(char *buffer, const char specifier, va_list args);
-int handle_unsigned_pointer(char *buffer, const char specifier, va_list args);
+int handle_integer_binary(char *buffer, const char specifier, va_list args, char flag);
+int handle_octal_hexa(char *buffer, const char specifier, va_list args, char flag);
+int handle_unsigned_pointer(char *buffer, const char specifier, va_list args, char flag);
 int process_format(const char *format, int *i, va_list args, char *buffer);
 int handle_custom_formats(char *buffer, const char specifier, va_list args);
-int handle_special_string(char *buffer, const char specifier, va_list args);
+int handle_special_string(char *buffer, const char specifier, va_list args, char flag);
 int _printf(const char *format, ...);
 
 #endif
