04e196c3fd42a533814321c03617d10006786d91
MemanAwad <meman1awad@gmail.com>
2023-09-19 12:46:52 +0200

    edit null

--- handle_numbers.c
+++ handle_numbers.c
@@ -130,13 +130,16 @@ int handle_unsigned_pointer(char *buffer, const char specifier, va_list args)
 
 				if (ptr == NULL)
 				{
-					tmp = "(null)";
+					tmp = "(nil)";
 				}
-				/* for 0x prefix */
-				buffer[index++] = '0';
-				buffer[index++] = 'x';
-				/* convert to hex*/
-				tmp = _utoa(adress, buffer + index, 16);
+				else
+				{
+					/* for 0x prefix */
+					buffer[index++] = '0';
+					buffer[index++] = 'x';
+					/* convert to hex*/
+					tmp = _utoa(adress, buffer + index, 16);
+				}
 				while (*tmp)
 				{
 					buffer[index++] = *tmp++;
