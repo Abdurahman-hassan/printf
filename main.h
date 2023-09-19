cb2787ccbf61a41d73b33fc384fdb49b67685664
MemanAwad <meman1awad@gmail.com>
2023-09-20 00:02:18 +0200

    add some function prototype

--- main.h
+++ main.h
@@ -19,12 +19,20 @@ char *_rot13(char *c);
 int reverse_string_with_len(char *buffer, char *str);
 void reverse_string_without_len(char *start, char *end);
 int handle_basic_formats(char *buffer, const char specifier, va_list args);
-int handle_integer_binary(char *buffer, const char specifier, va_list args, char flag);
-int handle_octal_hexa(char *buffer, const char specifier, va_list args, char flag);
-int handle_unsigned_pointer(char *buffer, const char specifier, va_list args, char flag);
+int handle_integer_binary(char *buffer, const char specifier,
+		va_list args, char flag);
+int handle_octal_hexa(char *buffer, const char specifier,
+		va_list args, char flag);
+int handle_unsigned_pointer(char *buffer, const char specifier,
+		va_list args, char flag);
 int process_format(const char *format, int *i, va_list args, char *buffer);
 int handle_custom_formats(char *buffer, const char specifier, va_list args);
-int handle_special_string(char *buffer, const char specifier, va_list args, char flag);
+char handle_flags(const char *format, int *i);
+int identity_specifier(const char *format, int *i, va_list arg,
+		char *buffer, char flag);
+int handle_buffer(char *buffer, int *buffer_size);
+int handle_special_string(char *buffer, const char specifier,
+		va_list args, char flag);
 int _printf(const char *format, ...);
 
 #endif
