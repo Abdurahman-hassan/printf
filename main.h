75d3fb8e7b804c25c3dde2ca3181d8b93b7c0be4
MemanAwad <meman1awad@gmail.com>
2023-09-22 20:20:38 +0200

    fix betty style

--- main.h
+++ main.h
@@ -10,15 +10,17 @@
 
 /* Buffer handling functions */
 int handle_buffer(char *buffer, int *buffer_index);
-int process_string(const char *format, char *buffer, va_list arg, int *total_printed);
+int process_string(const char *format, char *buffer,
+		va_list arg, int *total_printed);
 int handle_remaining_buffer(char *buffer, int buffer_index);
 
 /* Flag handling functions */
 char handle_flags(const char *format, int *i);
-int handle_width (int *i, char *format);
+int handle_width(int *i, char *format);
 int identify_specifier(const char *format, int *i,
 		va_list args, char *buffer, char flag, int *buff_index, int *total_printed);
-int process_format(const char *format, int *i, va_list args, char *buffer, int *buffer_index, int *total_printed);
+int process_format(const char *format, int *i, va_list args, char *buffer,
+		int *buffer_index, int *total_printed);
 
 /* Utility functions */
 char *_strchr(const char *s, int c);
@@ -33,7 +35,8 @@ int reverse_string_with_len(char *buffer, char *str);
 void reverse_string_without_len(char *start, char *end);
 
 /* Format handling functions */
-int handle_basic_formats(char *buffer, const char specifier, va_list args, int *buff_index, int *total_printed);
+int handle_basic_formats(char *buffer, const char specifier, va_list args,
+		int *buff_index, int *total_printed);
 int handle_custom_formats(char *buffer, const char specifier, va_list args);
 int handle_integer_binary(char *buffer, const char specifier,
 		va_list args, char flag);
@@ -44,7 +47,8 @@ int handle_unsigned_pointer(char *buffer, const char specifier,
 int handle_special_string(char *buffer, const char specifier,
 		va_list args, char flag);
 int print_charachter(char *buffer, va_list args);
-int print_string(va_list args, char *buffer, int *buffer_index, int *total_printed);
+int print_string(va_list args, char *buffer, int *buffer_index,
+		int *total_printed);
 
 /* Main printf function */
 int _printf(const char *format, ...);
