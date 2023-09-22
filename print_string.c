b2f860b82500700455a58b7aed41e9bec5beb509
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-09-22 17:34:15 +0000

    fix linting

--- print_string.c
+++ print_string.c
@@ -1,5 +1,18 @@
 #include "main.h"
 
+/**
+ * print_string - Prints a string to a buffer
+ * and flushes the buffer if full.
+ * This function prints a string to an output buffer.
+ * If the buffer reaches its capacity (as specified by BUFFER_SIZE),
+ * the buffer is written to the standard
+ * output and the buffer counters are reset.
+ * @args: Variable argument list containing the string to be printed.
+ * @buffer: The buffer in which the string is to be stored.
+ * @buffer_index: The current position index in the buffer.
+ * @total_printed: A counter to keep track of the total characters printed.
+ * Return: The number of characters added to the buffer by this function.
+ */
 int print_string(va_list args, char *buffer,
 		int *buffer_index, int *total_printed)
 {
