61a874075572ffcbc3fe34442bf92e938d6396e1
MemanAwad <meman1awad@gmail.com>
2023-09-23 00:15:19 +0200

    remove a check

--- _printf.c
+++ _printf.c
@@ -17,8 +17,6 @@ int _printf(const char *format, ...)
 		return (-1);
 	if (format[0] == '%' && !format[1])
 		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
 
 	va_start(args, format);
 
